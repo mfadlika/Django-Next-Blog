ae8bad031b5ecaacdb28d5e7dcb6954adc3e1640
mfadlika <mfadlika@outlook.com>
2023-03-18 17:35:08 +0700

    Blog

---
+++ client/src/pages/api/auth.ts
@@ -0,0 +1,9 @@
+async function signin(ip: string | string[], site: string) {
+  console.log("hello");
+}
+
+async function signup(ip: string | string[], site: string) {
+    console.log("hello");
+  }
+
+export default signin;
--- server/account/models.py
+++ server/account/models.py
@@ -1,10 +1,28 @@
 from django.db import models
-from django.core import validators
-from django.core.validators import MinLengthValidator, MaxValueValidator, MaxLengthValidator
+from django.contrib.auth.models import User
+from django.core.validators import MinLengthValidator
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.hashers import make_password
 
 # Create your models here.
 
+class Role(models.Model):
+    role_name = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.role_name
+
 class Account(models.Model):
-    name = models.CharField(max_length=20)
-    username = models.CharField(max_length=50)
-    password = models.CharField(max_length=50, validators=[MinLengthValidator[8]])
+    name = models.CharField(max_length=50)
+    username = models.CharField(max_length=20)
+    password = models.CharField(max_length=250)
+    email = models.EmailField(max_length=100)
+    role = models.ForeignKey(Role, on_delete=models.CASCADE)
+
+    def save(self):
+    # Save the provided password in hashed format
+        self.password = make_password(self.password)
+        super(Account, self).save()
+
+    def __str__(self):
+        return self.username
